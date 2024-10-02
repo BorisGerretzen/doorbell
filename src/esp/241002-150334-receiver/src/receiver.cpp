f768b166d9b146d603250fa73cca160e5d75b39b
Boris <borisgerretzen@gmail.com>
2024-10-02 21:39:39 +0200

    apapaspap

--- src/esp/241002-150334-receiver/src/receiver.cpp
+++ src/esp/241002-150334-receiver/src/receiver.cpp
@@ -1,34 +1,54 @@
 #include <Arduino.h>
-#define RFIN_PIN 11
+#define ADC_PRESCALE 16 // Set ADC clock prescaler (adjust to tune the speed)
 
-void rfDetect_ISR();
+// Peak detection variables
+#define THRESHOLD 850    // Threshold for distinguishing 0 and 1
+#define SUBTHRESHOLD 400 // Threshold for ignorigin mini peaks
+bool isRising = false;
+int lastSample = 0;
 
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(115200);
 
-  // rfTimer = timerBegin(0, 8, true);
-  // timerAttachInterrupt(rfTimer, &rfDetectTimer_ISR, true);
-  // timerAlarmWrite(rfTimer, STREAM_BITDETECT, false);
-  // timerAlarmDisable(rfTimer);
+  // Set up the ADC for faster sampling
+  ADCSRA &= ~((1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0)); // Clear prescaler bits
+  ADCSRA |= (1 << ADPS1) | (1 << ADPS0);  // Set prescaler to 8 for faster sampling (can be adjusted)
 
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(RFIN_PIN, INPUT_PULLUP);                 // RFIN_PIN configed as input pin
-  attachInterrupt(RFIN_PIN, rfDetect_ISR, CHANGE); //   and enable edge detection interrupt
-}
+  ADMUX = (1 << ADLAR) | (1 << REFS0);    // Left adjust result, use AVcc as reference
+  ADCSRA |= (1 << ADEN);                  // Enable the ADC
+  }
 
-void loop(){}
-
-void rfDetect_ISR()
+void loop()
 {
-  uint8_t pinState = digitalRead(RFIN_PIN); // Read the RFIN_PIN logical level
-  if (pinState == LOW)
-  { // if logic level is LOW, that is Falling Edge
-    digitalWrite(LED_BUILTIN, LOW);
+  // Start ADC conversion
+  ADCSRA |= (1 << ADSC);
+  
+  // Wait for conversion to complete
+  while (ADCSRA & (1 << ADSC))
+    ;
+
+  // Read the ADC value (10-bit, right-adjusted)
+  int sample = ADCH;  // Use ADCW to read both ADCL and ADCH combined
+
+  // ignore mini peaks
+  if(sample < SUBTHRESHOLD) {
+    lastSample = sample;
     return;
   }
 
-  digitalWrite(LED_BUILTIN, HIGH);
+  if(sample < lastSample && isRising) {
+    isRising = false;
+    if(lastSample > THRESHOLD) {
+      Serial.println("1");
+    } else {
+      Serial.println("0");
+    }
+  }
 
-  Serial.println("RF state " + String(pinState) + "\n");
+  if(sample > lastSample) {
+    isRising = true;
+  }
+
+  lastSample = sample;
 }
